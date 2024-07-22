aaba2201fe15ab3d2f98fd3fc967bea2629a013f
Samuel Kunle <samuelkunle215@gmail.com>
2024-07-23 00:14:50 +0100

    array input changed

--- src/main/java/JavaArray.java
+++ src/main/java/JavaArray.java
@@ -2,7 +2,7 @@ public class JavaArray {
     public static void main (String args []){
 
         int totalNum = 0;
-        int [] num = {23, 34, 54, 65, 56};
+        int [] num = {23, 67, 98, 65, 09};
         for(int i = 0; i < num.length; i++){
             //System.out.println(num[i]);
             totalNum = totalNum + num[i];
