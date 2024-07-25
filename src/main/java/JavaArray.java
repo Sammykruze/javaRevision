e641b6eac53ba78159bd2a70fd97043676877a23
Samuel Kunle <samuelkunle215@gmail.com>
2024-07-25 23:16:20 +0100

    new values added in the array

--- src/main/java/JavaArray.java
+++ src/main/java/JavaArray.java
@@ -2,7 +2,7 @@ public class JavaArray {
     public static void main (String args []){
 
         int totalNum = 0;
-        int [] num = {23, 67, 98, 65, 09};
+        int [] num = {23, 67, 98, 65, 89, 56, 57};
         for(int i = 0; i < num.length; i++){
             //System.out.println(num[i]);
             totalNum = totalNum + num[i];
