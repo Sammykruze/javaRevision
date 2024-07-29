b6b8170721e80f46e767a2775fae8c1ce1a32972
Samuel Kunle <samuelkunle215@gmail.com>
2024-07-29 01:04:50 +0100

    array variable name changed

--- src/main/java/JavaArray.java
+++ src/main/java/JavaArray.java
@@ -2,10 +2,10 @@ public class JavaArray {
     public static void main (String args []){
 
         int totalNum = 0;
-        int [] num = {23, 67, 98, 65, 89, 56, 57};
-        for(int i = 0; i < num.length; i++){
+        int [] nums = {23, 67, 98, 65, 89, 56, 57};
+        for(int i = 0; i < nums.length; i++){
             //System.out.println(num[i]);
-            totalNum = totalNum + num[i];
+            totalNum = totalNum + nums[i];
 
             System.out.println(totalNum);
         }
